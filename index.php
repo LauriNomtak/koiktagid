4ea9c2d5eb1f00fc8f58da334c7063504f7b3180
Lauri <lauri.nomtak@mail.ee>
2015-10-04 21:24:04 +0300

    koiktagid

---
+++ index.php
@@ -0,0 +1,259 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <base href="assets/" target="_blank">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <meta charset="UTF-8">
+    <title>Kõik tagid</title>
+    <!-- Kõikide tagide projekt-->
+</head>
+
+<style>
+ h1{color:whitesmoke;}
+</style>
+
+<body>
+<div id="header">
+    <header>
+        <h1>Kõikide tagide projekt</h1>
+    </header>
+
+    <nav id="nav">
+        <a href=" ">Nav 1</a> |
+        <a href=" ">Nav 2</a> |
+        <a href=" ">Nav 3</a> |
+        <a href="http:\\www.google.ee ">Nav 4</a>
+    </nav>
+</div>
+
+<br>
+
+<main>
+
+    <img id=html src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2000px-HTML5_logo_and_wordmark.svg.png" width="145" height="126" alt="html5"
+         usemap="#map">
+
+    <map name="map">
+        <area shape="rect" coords="145,25,0,0" href="https://en.wikipedia.org/wiki/HTML" alt="htmlwiki">
+    </map>
+
+    <figcaption><dfn>HTML</dfn> is the standard markup language for creating web pages.</figcaption>
+
+    <article>
+        <pre>            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Adipisci earum enim et
+            eum iste labore maxime nesciunt
+            perspiciatis, quae quaerat, quis soluta vel,
+            voluptate? Aspernatur beatae
+            consequuntur facere fugit modi.</pre>
+    </article>
+
+    <p><b>Lorem ipsum </b> dolor sit <sub>amet</sub>, <sup>consectetur</sup> adipisicing elit. <span style="color: red">Ad alias</span>, asperiores, at<wbr>consectetur cum deleniti dolor<wbr>
+        doloremque est illo <kbd>inventore</kbd> officiis omnis provident rem repellat sint totam, ullam voluptas! Amet architecto
+        asperiores corporis ex, explicabo <mark>fuga</mark> fugit impedit magnam, molestiae molestias, <abbr title="World Trade Organisation">WTO</abbr> recusandae rerum sit tempora
+        temporibus totam <del>voluptatem</del> <ins><bdo dir="rtl">voluptatem</bdo></ins>! Doloribus in molestias sint. Aliquam, assumenda, commodi cupiditate ea ex facilis ipsa
+        mollitia numquam optio perferendis quaerat repellat voluptates voluptatibus. Asperiores, expedita harum <i>laboriosam</i>
+        maiores quidem tenetur veniam <time>09:00</time> veritatis. A assumenda at   consequuntur doloribus ducimus error ex facilis, illum
+        impedit ipsam maxime minima omnis quas quis quod rerum sint voluptas, voluptate?</p>
+
+    <img id=pilt src="http://lorempixel.com/1000/200/technics">
+    <br>
+    <cite>The picture</cite> by <code>lorempixel</code>.
+
+    <hr>
+
+</main>
+
+<h2>Pealkiri II</h2>
+
+<aside>
+    <h4>Lorem ipsum.</h4>
+</aside>
+
+<dialog open>Dialoog</dialog>
+
+<ul id="list1">
+    <li class="item"><s>Lorem ipsum</s> <samp>dolor sit amet</samp>, consectetur adipisicing elit. Doloremque, eaque?</li>
+    <li class="item"><bdi><bdo dir="rtl">Lorem ipsum</bdo></bdi> amet ea explicabo iste itaque, necessitatibus nemo nihil nobis! Aperiam?</li>
+    <li class="item">A illum veritatis vitae. Ab aliquam impedit optio repellat temporibus.</li>
+    <li class="item">Consequatur consequuntur earum, excepturi facere incidunt maxime minima pariatur placeat.</li>
+</ul>
+
+<ul id="list2">
+    <li>A illum veritatis vitae. Ab aliquam impedit optio repellat temporibus.</li>
+    <li>Consequatur consequuntur earum, excepturi facere incidunt maxime minima pariatur placeat.</li>
+</ul>
+
+<figure>
+    <img src="http://lorempixel.com/400/200/technics" alt="The picture">
+    <figcaption>Lorem ipsum dolor sit amet.</figcaption>
+</figure>
+
+<section>
+    <h3>Pealkiri III</h3>
+    <ol class="generic-list">
+        <li class="item"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, sit.</strong></li>
+        <li class="item"><u>Assumenda cupiditate nam natus nihil? Delectus error nisi quisquam quo!</u></li>
+        <li class="item"><var>Aliquam</var> deserunt enim tempore? Minus nostrum omnis perspiciatis recusandae voluptas!</li>
+        <li class="item">Consequatur dignissimos odio reprehenderit sed. Assumenda consequuntur laudantium porro
+            voluptatum?
+        </li>
+    </ol>
+</section>
+
+<iframe width="650" height="280" src="https://www.youtube.com/embed/bWPMSSsVdPk" frameborder="0" allowfullscreen></iframe>
+
+<br>
+
+<textarea rows="4" cols="50">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto dignissimos dolores, doloribus illo obcaecati omnis ratione. Ab laudantium molestias tempora?
+</textarea>
+
+
+<dl>
+    <dt><em>Lorem</em></dt>
+    <dd>Lorem ipsum dolor sit amet, consectetur.</dd>
+    <dt><em>Dolor</em></dt>
+    <dd>Lorem ipsum dolor sit amet, consectetur adipisicing.</dd>
+</dl>
+
+<embed src="cubes.swf" autostart="off"></embed>
+
+<h4>Pealkiri IV</h4>
+
+<table>
+    <caption>Lorem ipsum dolor</caption>
+    <colgroup>
+        <col span="1" style="background-color:lavender">
+        <col style="background-color:darkseagreen">
+    </colgroup>
+    <thead>
+        <tr id="row1">
+            <td>Quae, sapiente!</td>
+            <td>Consequatur, ipsum?</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr id="row2">
+            <td>Nemo, sequi!</td>
+            <td>Consequatur, ipsum?</td>
+        </tr>
+        <tr id="row3">
+            <td>Dolore, quos.</td>
+            <td>Consequatur, ipsum?</td>
+        </tr>
+    </tbody>
+    <tfoot>
+        <tr id="row4">
+            <td>Aspernatur, dolor.</td>
+            <td>Consequatur, ipsum?</td>
+        </tr>
+    </tfoot>
+</table>
+
+<br>
+
+<audio controls>
+    <source src="Horse.mp3" type="audio/mpeg">
+</audio>
+
+<br>
+
+<video  width="700px" controls>
+    <source src="SampleVideo_1080x720_1mb.mp4">
+    <track src="text.txt" kind="subtitles" srclang="en" label="English">
+</video>
+
+<br>
+
+<form action="">
+
+    <fieldset>
+        Kuidas meeldis:<input list="tagasiside">
+        <datalist id="tagasiside">
+            <select>
+            <optgroup lable="Valikud:">
+            <option value="Suurepärane">
+            <option value="Väga hea">
+            <option value="Hea">
+            <option value="Rahuldav">
+            <option value="Ei oska öelda">
+            </optgroup>
+            </select>
+        </datalist><br>
+        <legend>Tagasiside:</legend>
+        Nimi: <input type="text"><br>
+        <label for="male">Mees</label>
+        <input type="radio" name="sex" id="male" value="male"><br>
+        <label for="female">Naine</label>
+        <input type="radio" name="sex" id="female" value="female"><br><br>
+        Email: <input type="text"><br>
+        <keygen name="security"><br>
+        <input type="submit" value="Saada">
+    </fieldset>
+    <meter value="0.3">30%</meter>
+    <br>
+    <progress value="30" max="100"></progress>
+</form>
+
+<h5>Pealkiri V</h5>
+
+<p>Quote from lorem ipsum</p>
+<blockquote cite="Lorem_ipsum">
+    <q>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, in!</q>
+</blockquote>
+
+<select>
+    <optgroup label="Lorem">
+        <option value="1">1</option>
+        <option value="2">2</option>
+    </optgroup>
+    <optgroup label="Ipsum">
+        <option value="3">1</option>
+        <option value="4">2</option>
+    </optgroup>
+</select>
+
+<form oninput="x.value=parseInt(a.value)*parseInt(b.value)">
+    <input type="number" id="a" value="50">
+    *<input type="number" id="b" value="50">
+    =<output name="x" for="a b"></output>
+</form>
+
+<button type="button">
+    Nupp
+</button>
+
+<br>
+
+<a href="http://www.google.ee">Google</a>
+<h6>Pealkiri VI</h6>
+
+<canvas id="myCanvas">Canvas</canvas>
+
+<div id="lehelopp">
+    <footer>
+        <address>Lauri Nõmtak</address>
+        <details>
+            <summary>2015</summary>
+            <small><p>VS15</p>
+            <p>Märgend- ja stiililehe keeled</p></small>
+        </details>
+    </footer>
+</div>
+
+<object data="Horse.mp3" type="audio/mpeg">
+    <param name="autoplay" value="false">
+</object>
+
+</body>
+
+<script>
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#AF0100";
+    ctx.fillRect(0, 0, 50, 50);
+</script>
+<noscript>Sinu veebilehitseja ei toeta JavaScripti!</noscript>
+
+</html>
